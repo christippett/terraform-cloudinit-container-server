bc0b3bcca977b6fbebbf3863753929f712b06205
Chris Tippett <c.tippett@gmail.com>
2020-07-26 08:36:27 +1000

    refactor: Update output from webhook

--- templates/webook/update-env.sh
+++ templates/webook/update-env.sh
@@ -3,8 +3,21 @@
 key="$1"
 value="$2"
 
-# replace environment variable
-sed -i "/^${key}=/s#.*#${key}=${value}#" /app/.env
+update_env() {
+    key="$1"
+    value="$2"
+    filename="$3"
 
-# return contents of .env, masking any sensitive values
-sed -E 's/^(.*?(KEY|SECRET).*?)=.*$/\1=**********/' /app/.env
+    sed -i "/^${key}=/s#.*#${key}=${value}#" "$filename"
+
+    # return contents of env file, masking any sensitive values
+    printf "[START %s]\n" "$filename"
+    sed -E 's/^(.*?(KEY|SECRET).*?)=.*$/\1=**********/' "$filename"
+    printf "[END %s]\n\n" "$filename"
+}
+
+# replace environment variable
+update_env "$key" "$value" /app/.env
+update_env "$key" "$value" /app/app.env
+
+printf "Update complete\n"
